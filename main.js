7e312e4123ca7298e4dd1b27733c6ddb50560f25
meghans18 <saakmeghan@gmail.com>
2018-04-11 11:30:56 -0500

    Initial Commit

---
+++ main.js
@@ -0,0 +1,38 @@
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+const url = require('url')
+const ipcMain = require('electron').ipcMain
+
+let main_win
+
+function createWindow() {
+    main_win = new BrowserWindow({width: 1000, height: 1000, backgroundColor: 'black', frame: false, fullscreen:true})
+    main_win.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file',
+        slashes: true
+    }))
+    let nowShowing = new BrowserWindow({parent: main_win, show: false, fullscreen: true, frame: false})
+    nowShowing.loadURL(url.format({
+        pathname: path.join(__dirname, 'nowShowing.html'),
+        protocol: 'file:',
+        slashes: true
+    }))
+
+    main_win.webContents.openDevTools()
+    nowShowing.webContents.openDevTools()
+
+    main_win.on('closed', () => {
+        main_win = null
+    })
+    
+    ipcMain.on('closeShowing', function() {
+        nowShowing.hide()
+    })
+
+    ipcMain.on('openShowing', function() {
+        nowShowing.show()
+    })
+}
+
+app.on('ready', createWindow)
